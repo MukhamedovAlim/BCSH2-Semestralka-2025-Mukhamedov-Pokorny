dcb19292828fcf3853093154f3e51e89b529cfce
Alim <alim2003@email.cz>
2025-10-09 11:01:44 +0200

    pridan member

---
+++ BCSH2-Semestralka-2025-Mukhamedov-Pokorny/src/FitnessCenter.Domain/Entities/Member.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BCSH2_Semestralka_2025_Mukhamedov_Pokorny.src.FitnessCenter.Domain.Entities;
+public sealed class Member
+{
+    public int MemberId { get; set; }
+    public string FirstName { get; set; } = "";
+    public string LastName { get; set; } = "";
+    public string Email { get; set; } = "";
+    public string? Phone { get; set; }
+    public DateTime? BirthDate { get; set; }
+    public bool IsActive { get; set; } = true;
+    public DateTime CreatedAt { get; set; }
+}
---
+++ BCSH2-Semestralka-2025-Mukhamedov-Pokorny/src/FitnessCenter.Web/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BCSH2_Semestralka_2025_Mukhamedov_Pokorny.src.FitnessCenter.Web
+{
+    internal class Program
+    {
+    }
+}
