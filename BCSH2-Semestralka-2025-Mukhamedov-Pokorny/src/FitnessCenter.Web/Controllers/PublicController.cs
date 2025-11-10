890badc927e5bf78bdb4a03683e11a02082b100f
David Pokorný <st72562@upce.cz>
2025-11-10 02:56:18 +0100

    neregistrovany uzivatel -> pohled

---
+++ BCSH2-Semestralka-2025-Mukhamedov-Pokorny/src/FitnessCenter.Web/Controllers/PublicController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Oracle.ManagedDataAccess.Client;
+using FitnessCenter.Infrastructure.Persistence; // DatabaseManager
+
+namespace FitnessCenter.Web.Controllers
+{
+    [AllowAnonymous]
+    public class PublicController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Active = "Public";
+            return View();
+        }
+
+        // Veřejný rozvrh – bez osobních údajů, jen název, datum, volná místa
+        public async Task<IActionResult> Lessons()
+        {
+            var list = new List<(int Id, string Nazev, DateTime Datum, int Volno)>();
+            try
+            {
+                using var con = await DatabaseManager.GetOpenConnectionAsync();
+                using var cmd = new OracleCommand(@"
+                    SELECT idlekce, nazevlekce, datumlekce, volno
+                    FROM v_lekce_volne
+                    WHERE datumlekce >= SYSDATE
+                    ORDER BY datumlekce", (OracleConnection)con);
+                using var rd = await cmd.ExecuteReaderAsync();
+                while (await rd.ReadAsync())
+                {
+                    list.Add((rd.GetInt32(0), rd.GetString(1), rd.GetDateTime(2), rd.GetInt32(3)));
+                }
+            }
+            catch { /* veřejná stránka – chybu nevyhazuj, jen zobraz prázdné */ }
+
+            return View(list);
+        }
+
+        // Příklad: veřejný seznam trenérů (jen jméno; bez kontaktů)
+        public async Task<IActionResult> Trainers()
+        {
+            var list = new List<string>();
+            try
+            {
+                using var con = await DatabaseManager.GetOpenConnectionAsync();
+                using var cmd = new OracleCommand(@"
+                    SELECT (jmeno || ' ' || prijmeni) 
+                    FROM treneri
+                    ORDER BY prijmeni, jmeno", (OracleConnection)con);
+                using var rd = await cmd.ExecuteReaderAsync();
+                while (await rd.ReadAsync()) list.Add(rd.GetString(0));
+            }
+            catch { }
+            return View(list);
+        }
+    }
+}
